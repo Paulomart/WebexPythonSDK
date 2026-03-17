9d43821830e05d05fb0e607252b1548eb08ea487
Tufan Kartal <Tufan.Kartal@persona.de>
2026-03-17 09:32:30 +0100

    add createHuntGroup
    (cherry picked from commit 369b3cfd38a372f4d98f90eac74cd8b50068ffc7)

--- src/webexpythonsdk/api/__init__.py
+++ src/webexpythonsdk/api/__init__.py
@@ -57,6 +57,7 @@ from .meetings import MeetingsAPI
 from .meeting_templates import MeetingTemplatesAPI
 from .meeting_invitees import MeetingInviteesAPI
 from .meeting_registrants import MeetingRegistrantsAPI
+from .hunt_groups import HuntGroupsAPI
 
 import os
 
@@ -254,6 +255,7 @@ class WebexAPI(object):
         self.meeting_registrants = MeetingRegistrantsAPI(
             self._session, object_factory
         )
+        self.hunt_groups = HuntGroupsAPI(self._session, object_factory)
 
     @property
     def access_token(self):
